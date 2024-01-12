ddccdd62c972f8081d8337dc8d6bf3ee02b06d3b
Anik Barua <anikbb05@gmail.com>
2024-01-12 20:58:35 +0600

    implement shad modal and dropdown

---
+++ src/components/todo/FilterTodo.tsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import { Button } from "../ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "../ui/dropdown-menu";
+
+const FilterTodo = () => {
+  const [position, setPosition] = useState("bottom");
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        {/* <Button variant="outline">Open</Button> */}
+        <Button className="bg-primary-gradient text-xl font-medium font-serif">
+          Filter Todo
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-56">
+        <DropdownMenuLabel>Panel Position</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuRadioGroup value={position} onValueChange={setPosition}>
+          <DropdownMenuRadioItem value="top">Top</DropdownMenuRadioItem>
+          <DropdownMenuRadioItem value="bottom">Bottom</DropdownMenuRadioItem>
+          <DropdownMenuRadioItem value="right">Right</DropdownMenuRadioItem>
+        </DropdownMenuRadioGroup>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
+
+export default FilterTodo;
--- src/components/todo/TodoContainer.tsx
+++ src/components/todo/TodoContainer.tsx
@@ -1,16 +1,20 @@
-import { Button } from "../ui/button";
+// import { Button } from "../ui/button";
+import AddTodoModal from "./AddTodoModal";
+import FilterTodo from "./FilterTodo";
 import TodoCard from "./TodoCard";
 
 const TodoContainer = () => {
   return (
     <div className="mt-1">
       <div className="flex justify-between mb-5">
-        <Button className="bg-primary-gradient text-xl font-medium font-serif">
+        {/* <Button className="bg-primary-gradient text-xl font-medium font-serif">
           Add Todo
-        </Button>
-        <Button className="bg-primary-gradient text-xl font-medium font-serif">
+        </Button> */}
+        <AddTodoModal />
+        {/* <Button className="bg-primary-gradient text-xl font-medium font-serif">
           Filter Todo
-        </Button>
+        </Button> */}
+        <FilterTodo />
       </div>
       <div className="bg-primary-gradient w-full h-full mt-1 rounded-md p-[5px] space-y-5">
         {/* <div className="bg-white p-3 text-center font-semibold text-2xl font-mono">
