5b55425c614d34429ce083a72afe0f25e04cae28
Anik Barua <anikbb05@gmail.com>
2024-01-14 00:01:04 +0600

    fix

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import todoReducer from "./features/todo/todoSlice";
+import { todoApi } from "@/api/api";
 
 export const store = configureStore({
   reducer: {
     todo: todoReducer,
+    [todoApi.reducerPath]: todoApi.reducer,
   },
 });
 
